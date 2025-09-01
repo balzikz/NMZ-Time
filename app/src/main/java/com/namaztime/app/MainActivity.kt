213b85623c2961750d930291ce95a8a40717aed7
balzikz <alikkhhhannnn@gmail.com>
2025-09-01 11:27:04 +0500

    Create MainActivity.kt

---
+++ app/src/main/java/com/namaztime/app/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.namaztime.app
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val textView = TextView(this)
+        textView.text = "Ассаламу алейкум!\nМоё первое приложение на Kotlin"
+        textView.textSize = 22f
+
+        setContentView(textView)
+    }
+}
